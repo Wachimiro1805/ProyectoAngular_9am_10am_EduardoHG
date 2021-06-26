a9b15a93fd470815e18dab1690bafffdc69f1865
edhernandezga <80096340+Wachimiro1805@users.noreply.github.com>
2021-06-25 19:10:49 -0600

    Enter

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyBPtNb8uhwe8l47HDu4bIaF2Vhx_9NlcO0",
+    authDomain: "empleados-2c152.firebaseapp.com",
+    projectId: "empleados-2c152",
+    storageBucket: "empleados-2c152.appspot.com",
+    messagingSenderId: "996944263733",
+    appId: "1:996944263733:web:7282bda21a8cb44ce40f7c"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyBPtNb8uhwe8l47HDu4bIaF2Vhx_9NlcO0",
+    authDomain: "empleados-2c152.firebaseapp.com",
+    projectId: "empleados-2c152",
+    storageBucket: "empleados-2c152.appspot.com",
+    messagingSenderId: "996944263733",
+    appId: "1:996944263733:web:7282bda21a8cb44ce40f7c"
+  }
 };
 
 /*
